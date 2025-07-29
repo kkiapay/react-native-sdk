d7f5c29061c677a60fe1b5e4e8ad6defc06c5188
HOUNKPATIN Kafui Terry Chris <114687214+HKafuiEPI@users.noreply.github.com>
2025-07-29 16:49:41 +0100

    Update linking.ts

--- src/linking.ts
+++ src/linking.ts
@@ -3,16 +3,6 @@ import { WAVE_STORE_REDIRECT_URI, APP_STORE_REDIRECT_URI } from './typings';
 
 export const launchWave = async (url: string): Promise<boolean> => {
   try {
-    const canOpen = await Linking.canOpenURL(url);
-    if (canOpen) {
-      const storeUrl =
-        Platform.OS === 'ios'
-          ? APP_STORE_REDIRECT_URI
-          : WAVE_STORE_REDIRECT_URI;
-      await Linking.openURL(storeUrl);
-      return true;
-    } 
-
     await Linking.openURL(url);
     return true;
     
